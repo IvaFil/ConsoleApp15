5d0298cf062c5ea55f43dfcc119140b81c84d047
IvaFil <filipovichiv@yandex.ru>
2022-10-09 18:25:05 +0300

    Задача 66

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -10,6 +10,33 @@ namespace ConsoleApp15
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите число M: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите число N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            SumMToN(m, n);
+
+            
+            void SumMToN(int m, int n)
+            {
+                Console.Write(SumMN(m - 1, n));
+            }
+
+            
+            int SumMN(int m, int n)
+            {
+                int res = m;
+                if (m == n)
+                    return 0;
+                else
+                {
+                    m++;
+                    res = m + SumMN(m, n);
+                    return res;
+                }
+            } 
 
         }
     }
